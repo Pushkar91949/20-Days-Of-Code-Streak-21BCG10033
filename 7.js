dfa6a319afa66ad1e96ee002487c0a75b4c31239
Pushkar91949 <117620508+Pushkar91949@users.noreply.github.com>
2023-01-15 11:27:19 +0530

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,13 @@
+function minMax(arr) {
+	let max=arr[0];
+	let min=arr[0];
+	for(let i = 0; i < arr.length; i++) {
+      if(arr[i] > max) {
+         max = arr[i];
+      }
+      else if (arr[i] < min) {
+         min = arr[i];
+      }
+   }
+	return([min,max]);
+}
